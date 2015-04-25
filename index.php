1438d718002d7559211dec319534752053cf90c1
jinyuwang <jinyu345@163.com>
2015-04-25 17:16:27 +0800

    commit-files-20150425
    commit-files-20150425

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * test
+ * @author kenny
+ * @date   2015-04-16
+ */
+echo date('Y-m-d h:i:s',1378998000)."<br/>";
+phpinfo();
---
+++ practise/html5-16.php
@@ -0,0 +1,20 @@
+
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hadoop
+ * Date: 2015/4/19
+ * Time: 13:42
+ * Title:HTML5系列 第十六集 HTML5表单与PHP交互
+ * URL:http://my.tv.sohu.com/pl/8387912/77881397.shtml
+ */
+echo "HTML5系列 第十六集 HTML5表单与PHP交互<br/>";
+//echo "用户名：".$_GET['name'].'<br/>密码：'.$_GET['password']."<br/>";
+echo "用户名：".$_POST['name1'].'<br/>密码：'.$_POST['password1']."<br/>";
+echo "GET方法可做资源定位；post无法做资源定位。";
+?>
+</html>
