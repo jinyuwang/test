46704eeffc7e1d70deebb0f987601289b1810a2f
jinyuwang <jinyu345@163.com>
2015-04-26 16:21:33 +0800

    Revert "remove testmysql.php"
    This reverts commit d1d89975ba71bf2bb2184d3959eab7685a189d9f.

---
+++ testmysql.php
@@ -0,0 +1,14 @@
+<?php 
+$link = mysqli_connect('localhost','test','test','test'); 
+if (!$link) { 
+	die('Could not connect to MySQL: ' . mysql_error()); 
+} 
+echo 'Connection OK'; mysqli_close($link); 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+echo 'Connection OK<br/>'; 
+?> 
