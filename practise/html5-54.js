9c314b8ee792b7bbfab5b2c4e3610fb788a01495
jinyuwang <jinyu345@163.com>
2015-05-07 23:17:40 +0800

    div,css,js
    update

---
+++ practise/html5-54.js
@@ -0,0 +1,28 @@
+document.write("<p>hello,I am kenny. This line is from JS!</p>");
+document.write("<h1>hello,I am kenny. This line is from JS!</h1>");
+//
+/*
+*
+*
+*
+*
+*
+* */
+var x=10;
+var x$1=10;
+var y=0.1;
+var z=3.1415926;
+var xy=x+y;
+var tf=true;
+var arr=[11,12,14,22,43];
+var ar1=new Array("hello","as","I","am","kenny");
+var ar2=new Array();
+ar2[0]=1;
+ar2[1]=2;
+ar2[2]=3;
+var n=null;
+var r;
+r=10;
+r=null;
+document.write("x$1=",x$1,"<br/>");
+document.write(arr[0],"<br/>",arr[2]);
