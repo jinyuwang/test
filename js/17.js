98ee53db4e75922d21a24e444f20c5a4ff613706
jinyuwang <jinyu345@163.com>
2015-05-18 20:38:52 +0800

    Revert "Revert "js-20150517""
    This reverts commit bb27ee1a90266d01264b5e56dee388fcf98048f7.

---
+++ js/17.js
@@ -0,0 +1,20 @@
+/*
+* if(condition1)
+* {}
+* else if(condition2)
+* {}
+* else
+* {}
+*
+* switch(表达式n)
+* {
+* case 1:
+* { ... }
+* break;
+*case 2:
+* { ... }
+* break;
+* default:
+* { ... }
+* }
+* */
