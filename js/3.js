bb27ee1a90266d01264b5e56dee388fcf98048f7
jinyuwang <jinyu345@163.com>
2015-05-18 20:38:29 +0800

    Revert "js-20150517"
    This reverts commit 38a0582ad87404ebe33278882bd5f4d0214b29c1.

--- js/3.js
+++
@@ -1,7 +0,0 @@
-document.write("<h1>这是一个标题</h1>");
-document.write("<p>这是一个段落</p>");
-function myFunction1()
-{
-    document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
-    document.getElementById("demo").style.color="#0088ff";
-}
