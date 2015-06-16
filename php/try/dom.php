6ace224f55bc1dae7dbd400cd68fda60bfc9622c
jinyuwang <jinyu345@163.com>
2015-06-16 11:30:31 +0800

    medoo
    medoo v0.9.6+update(pdo事务，debug)

---
+++ php/try/dom.php
@@ -0,0 +1,21 @@
+<html>
+<title>test</title><meta content="text/html" charset="UTF-8">
+<body>
+<form>
+    <hr/>
+    <ol>
+        <li>aaa</li>
+        <li>bbb</li>
+        <li>ccc</li>
+        <li>ddd</li>
+    </ol>
+</form>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hadoop
+ * Date: 2015/6/15
+ * Time: 22:08
+ */
